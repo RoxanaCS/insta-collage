00b7a1e288f7f65faa7e7b2655973d5e2108d0fd
RoxanaCS <roxale.cares@gmail.com>
2018-02-07 17:10:40 -0300

    validación de contraseña

--- src/js/app.js
+++ src/js/app.js
@@ -1,28 +1,41 @@
 $(document).ready(function() {
+  $('#collage').hide();
+  $('#photo-booth').hide();
+  $('#photos').hide();
   images();
 });
+
+
+$('#button').click(function() {
+  if ($('#password').val() === '' || $('#password').val() === '123456' || $('#password').val().length < 5) {
+    alert('Ingrese una contraseña valida');
+  } else {
+    $('#collage').show();
+    $('#photo-booth').show();
+    $('#photos').show();
+    $('#init').hide();
+    $('footer').hide();
+  }
+});
+
 function drag(ev) {
   console.log(ev.target.src);
-  console.log(ev.target.getAttribute("data-img"))
+  console.log(ev.target.getAttribute('data-img'));
   ev.dataTransfer.setData('text', ev.target.id);
 }
-
-function permitirDrop(ev){
+function permitirDrop(ev) {
   ev.preventDefault();
 }
-
 function drop(ev) {
   ev.preventDefault();
-  var photoId = ev.dataTransfer.getData('text'); 
+  var photoId = ev.dataTransfer.getData('text');
   var photo = document.getElementById(photoId);
-  var photoSrc = photo.getAttribute("data-img");
+  var photoSrc = photo.getAttribute('data-img');
   ev.target.style.backgroundImage = "url('" + photoSrc + "')";
 }
-
-
-function images(){
-  var images = $(".imageDiv")
-  images.each(function(){
+function images() {
+  var images = $('.imageDiv');
+  images.each(function() {
     $(this).css('background-image', 'url(' + $(this).attr('data-img') + ')');
-  })
+  });
 }
